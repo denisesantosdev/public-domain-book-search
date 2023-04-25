673f29d12c5c6fa2f74740e3b7729bc7ed2ecf1f
denisesantosdev <denisesantosdev@gmail.com>
2023-04-24 22:53:29 -0300

    create function to retrieve book info from open library

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -9,3 +9,17 @@
 }
 
 searchEbook('Pride and Prejudice') */
+import { getBookKey } from "./services/book-key.js";
+
+let bookKey = await getBookKey('Pride and Prejudice')
+console.log(bookKey);
+
+async function getBookInfo(bookKey) {
+    const url = `https://openlibrary.org${bookKey}.json`
+
+    const response = await fetch(url)
+    const data = await response.json()
+    console.log(data);
+}
+
+getBookInfo(bookKey)
